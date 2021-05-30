921195884f75763b69cc29444396493157e831c3
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-05-30 01:35:59 -0400

    removed typo from import

--- vote_system/tests/unit/test_admin_api.py
+++ vote_system/tests/unit/test_admin_api.py
@@ -1,16 +1,19 @@
 import pytest
 
 from core.abstract_data_access_layer import AbstractDataAccessLayer
+from core.api_factory import ApiFactory
 from core.admin_api import AdminApi
 from core.models import User
 from core.models import Candidate
 from core.models import Election
 
 import pytest
+from loguru import logger
 
 MOCK_NEW_USER_ID = 1
 MOCK_NEW_ELECTION_ID = 1
 MOCK_NEW_CANDIDATE_ID = 50
+
 
 @pytest.fixture
 def data_access_layer(mocker):
@@ -21,4 +24,16 @@ def data_access_layer(mocker):
     return dal
 
 def test_create_user(data_access_layer):
+    # arrange
+    api_factory = ApiFactory(
+        token={},
+        data_access_layer=data_access_layer,
+        logger=logger
+    )
+    admin = api_factory.create_admin_api()
 
+    # act
+    new_user_id = admin.create_user()
+
+    # assert
+    assert new_user_id == MOCK_NEW_USER_ID
