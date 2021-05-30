a8290464804e6fb1f7de861c945845206d18b0d5
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-05-30 00:14:02 -0400

    Added a logger to BaseApi dependencies

--- vote_system/app/core/base_api.py
+++ vote_system/app/core/base_api.py
@@ -5,16 +5,19 @@ from core.abstract_data_access_layer import AbstractDataAccessLayer
 
 class BaseApi(ABC):
 
-    def __init__(self, token: dict, data_access_layer: AbstractDataAccessLayer) -> None:
+    def __init__(self, token: dict, data_access_layer: AbstractDataAccessLayer, logger) -> None:
         """
         The Api's constructor should be used to determine that a user is a valid admin.
 
         Args:
             token: dictionary containing required credentials expected
                    for an admin user.
+            data_access_layer: an implementation of AbstractDataAccessLayer
+            logger: object with logging calls
 
         Return:
             None
         """
+        self._log = logger
         self._dal = data_access_layer
         self._token = token
