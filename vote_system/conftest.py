9261f4a9d6f7d6b1cabb80918ce6cd11cb464de5
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-05-30 01:27:37 -0400

    fixed conftext issues with path

--- vote_system/conftest.py
+++ vote_system/conftest.py
@@ -1,8 +1,6 @@
 import sys, os
 
 # Add the app path to sys.path
-# Since conftest.py is in root, we'll add <root>/app/
 sys.path.insert(0, 
-    os.path.dirname(os.path.abspath(__file__))
-    + '/../app'
+    os.path.dirname(os.path.abspath(__file__)) + '/app'
 )
