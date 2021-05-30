e3555089134553e20a8dd22a67543915586b0b9d
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-05-29 23:58:46 -0400

    removed accidental copy of admin api implementation

--- vote_system/app/core/user_api.py
+++ vote_system/app/core/user_api.py
@@ -3,32 +3,4 @@ from core.base_api import BaseApi
 
 class UserApi(BaseApi):
 
-    def create_user(self, username: str) -> int:
-        """Create new user and return new user's ID"""
-        return self._dal.create_user(username=username)
-    
-    def create_candidate(self, username: str) -> int:
-        """Adds candidate flag to username
-
-        If the username does not exist, a new one user will be created.
-
-        Return:
-            int representing the user id of the candidate that was created.
-        """
-        return self._dal.create_candidate(username=username)
-
-    def create_election(self, election_name: str) -> int:
-        """Creates a new election
-
-        Return:
-            id of newly created election
-        """
-        return self._dal.create_election(election_name=election_name)
-
-    def declare_winner(election_id: int) -> int:
-        """Prematurely ends election
-
-        Return:
-            id of candidate that has won the election.
-        """
-        raise NotImplementedError()
+ 
