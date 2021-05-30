3e68ae2fd448804d0198e9d45753c7d42afca54f
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-05-30 00:19:17 -0400

    Added logging to api factory

--- vote_system/app/core/api_factory.py
+++ vote_system/app/core/api_factory.py
@@ -6,18 +6,19 @@ from core.admin_api import AdminApi
 
 class ApiFactory:
 
-    def __init__(self, token: dict, data_access_layer: AbstractDataAccessLayer) -> None:
+    def __init__(self, token: dict, data_access_layer: AbstractDataAccessLayer, logger) -> None:
         self._token = token
         self._dal = data_access_layer
+        self._log = logger
 
     def create_voter_api(self) -> VoterApi:
         #TODO validate token for VOTERS
-        return VoterApi(token=self._token, data_access_layer=self._dal)
+        return VoterApi(token=self._token, data_access_layer=self._dal, logger=self._log)
     
     def create_admin_api(self) -> AdminApi:
         #TODO validate token for ADMINS
-        return AdminApi(token=self._token, data_access_layer=self._dal)
+        return AdminApi(token=self._token, data_access_layer=self._dal, logger=self._log)
     
     def create_user_api(self) -> UserApi:
         #TODO validate token for REGISTERED USERS
-        return UserApi(token=self._token, data_access_layer=self._dal)
+        return UserApi(token=self._token, data_access_layer=self._dal, logger=self._log)
