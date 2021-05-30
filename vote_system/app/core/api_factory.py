11dc5ff56aa5c3d6ef118b6a52d617bf64d2f624
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-05-29 23:59:24 -0400

    created ApiFactory

---
+++ vote_system/app/core/api_factory.py
@@ -0,0 +1,23 @@
+from core.abstract_data_access_layer import AbstractDataAccessLayer
+from core.voter_api import VoterApi
+from core.user_api import UserApi
+from core.admin_api import AdminApi
+
+
+class ApiFactory:
+
+    def __init__(self, token: dict, data_access_layer: AbstractDataAccessLayer) -> None:
+        self._token = token
+        self._dal = data_access_layer
+
+    def create_voter_api(self) -> VoterApi:
+        #TODO validate token for VOTERS
+        return VoterApi(token=self._token, data_access_layer=self._dal)
+    
+    def create_admin_api(self) -> AdminApi:
+        #TODO validate token for ADMINS
+        return AdminApi(token=self._token, data_access_layer=self._dal)
+    
+    def create_user_api(self) -> UserApi:
+        #TODO validate token for REGISTERED USERS
+        return UserApi(token=self._token, data_access_layer=self._dal)
