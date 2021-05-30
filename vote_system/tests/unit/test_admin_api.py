eea9f75ddbe26782786118779d410edb240de6a7
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-05-30 12:28:06 -0400

    added create candidate and election tests. updated import order

--- vote_system/tests/unit/test_admin_api.py
+++ vote_system/tests/unit/test_admin_api.py
@@ -1,4 +1,5 @@
 import pytest
+from loguru import logger
 
 from core.abstract_data_access_layer import AbstractDataAccessLayer
 from core.api_factory import ApiFactory
@@ -7,8 +8,6 @@ from core.models import User
 from core.models import Candidate
 from core.models import Election
 
-import pytest
-from loguru import logger
 
 MOCK_NEW_USER_ID = 1
 MOCK_NEW_ELECTION_ID = 1
@@ -16,24 +15,38 @@ MOCK_NEW_CANDIDATE_ID = 50
 
 
 @pytest.fixture
-def data_access_layer(mocker):
+def api(mocker):
     dal = mocker.Mock(spec=AbstractDataAccessLayer)
     dal.create_user.return_value = MOCK_NEW_USER_ID
     dal.create_election.return_value = MOCK_NEW_ELECTION_ID
     dal.create_candidate.return_value = MOCK_NEW_CANDIDATE_ID
-    return dal
 
-def test_create_user(data_access_layer):
-    # arrange
     api_factory = ApiFactory(
         token={},
-        data_access_layer=data_access_layer,
+        data_access_layer=dal,
         logger=logger
     )
-    admin = api_factory.create_admin_api()
+    return api_factory.create_admin_api()
 
+
+def test_create_user(api):
     # act
-    new_user_id = admin.create_user('Test User')
+    new_user_id = api.create_user('Test User')
 
     # assert
     assert new_user_id == MOCK_NEW_USER_ID
+
+
+def test_create_candidate(api):
+    # act
+    candidate_id = api.create_candidate(username='some candidate')
+
+    # assert
+    assert candidate_id == MOCK_NEW_CANDIDATE_ID
+
+def test_create_election(api):
+    # act
+    election_id = api.create_election(election_name='City Council Election')
+
+    # assert
+    assert election_id == MOCK_NEW_ELECTION_ID
