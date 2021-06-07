93d2c5538a5711e611ecd519005e8e07ba510b89
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-06-06 21:45:49 -0400

    black formatting, addition for req'd params

--- vote_system/tests/integration/test_admin_presentation.py
+++ vote_system/tests/integration/test_admin_presentation.py
@@ -3,11 +3,9 @@ from presentation.app import app
 import pytest
 
 
-ADMIN_HEADERS = {
-    "x-access-token": {
-        "userType": "admin"
-    }
-}
+ADMIN_HEADERS = {"x-access-token": {"userType": "admin"}}
+VOTER_HEADERS = {"x-access-token": {"userType": "voter"}}
+
 
 @pytest.fixture
 def client():
@@ -16,20 +14,26 @@ def client():
 
 
 def test_create_user(client):
-    response = client.post("user", headers=ADMIN_HEADERS)
+    response = client.post("user", headers=ADMIN_HEADERS, json={"username": "newuser"})
     assert response.status_code == 200
 
 
 def test_create_vote(client):
-    response = client.post("vote", headers=ADMIN_HEADERS)
+    response = client.post(
+        "vote", headers=VOTER_HEADERS, json={"vote": [3, 4, 5, 1, 5]}
+    )
     assert response.status_code == 200
 
 
 def test_create_candidate(client):
-    response = client.post("candidate", headers=ADMIN_HEADERS)
+    response = client.post(
+        "candidate", headers=ADMIN_HEADERS, json={"username", "random candidate"}
+    )
     assert response.status_code == 200
 
 
 def test_create_election(client):
-    response = client.post("election", headers=ADMIN_HEADERS)
+    response = client.post(
+        "election", headers=ADMIN_HEADERS, json={"electionName": "city council 2021"}
+    )
     assert response.status_code == 200
