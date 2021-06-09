ea418df1070222162c814cbbf55e86b73e845797
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-06-09 00:13:24 -0400

    JWT token issue solved in tests

--- vote_system/tests/integration/test_admin_presentation.py
+++ vote_system/tests/integration/test_admin_presentation.py
@@ -1,20 +1,47 @@
-from presentation.app import app
+from base64 import b64encode
 
 import pytest
+from flask_jwt_extended import create_access_token
+
+from presentation.app import app
 
 
-ADMIN_HEADERS = {"x-access-token": {"userType": "admin"}}
+credentials = b64encode(b"test_user:test_password").decode("utf-8")
+
+
+def get_admin_headers(token):
+    return {
+        "x-access-token": {"userType": "admin"},
+        "Authorization": f"Bearer {token}",
+    }
+
+
 VOTER_HEADERS = {"x-access-token": {"userType": "voter"}}
 
 
 @pytest.fixture
 def client():
-    app.testing = True
+    app.config["TESTING"] = True
     return app.test_client()
 
 
-def test_create_user(client):
-    response = client.post("user", headers=ADMIN_HEADERS, json={"username": "newuser"})
+@pytest.fixture
+def token(client):
+    response = client.post("/login", json={"username": "test", "password": "test"})
+    token = response.json["access_token"]
+    return token
+
+
+def test_index_loads(client):
+    response = client.get("/")
+    assert response.data == b"SUCCESS"
+    assert response.status_code == 200
+
+
+def test_create_user(client, token):
+    response = client.post(
+        "/user", headers=get_admin_headers(token), json={"username": "newuser"}
+    )
     assert response.status_code == 200
 
 
