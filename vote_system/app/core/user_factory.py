d03f676f28b9fdbe56dab3e96875f0f0ad3ce0da
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-06-19 13:40:19 -0400

    implemented user factory

--- vote_system/app/core/user_factory.py
+++ vote_system/app/core/user_factory.py
@@ -1,4 +1,5 @@
 from .abstract_data_access_layer import AbstractDataAccessLayer
+from .models import User, Candidate, Voter
 
 
 class UserFactory:
@@ -6,5 +7,15 @@ class UserFactory:
         self._dal = data_access_layer
         self._log = logger
 
-    def create_new_user(self, username: id) -> int:
-        return self._dal.create_user(username=username)
+    def get_user(self, user_id: int) -> User:
+        user_info = self._dal.get_user_info(id=user_id)
+        return self._create_user(
+            user_id=user_info["id"],
+            username=user_info["username"],
+            phone_number=user_info["phone_number"],
+            email_address=user_info["email_address"]
+        )
+
+    def _create_user(self, user_id: int, username: str, user_type: str, phone_number: str, email: str) -> User:
+        if user_type == 'user':
+            return User(id=user_id, name=username, phone_number=phone_number, email_address=email)
