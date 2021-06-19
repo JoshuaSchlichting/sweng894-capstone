ce21864c619b313d2a9a11c9e780abcdb3292292
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-06-19 13:39:51 -0400

    constructor update

--- vote_system/app/core/api_factory.py
+++ vote_system/app/core/api_factory.py
@@ -7,24 +7,24 @@ from .models import User
 
 class ApiFactory:
     def __init__(
-        self, user: User, data_access_layer: AbstractDataAccessLayer, logger
+        self, user_id: int, data_access_layer: AbstractDataAccessLayer, logger
     ) -> None:
-        self._user = user
+        self._user_id = user_id
         self._dal = data_access_layer
         self._log = logger
 
     def create_voter_api(self) -> VoterApi:
         # TODO validate token for VOTERS
         return VoterApi(
-            user_id=self._user.id, data_access_layer=self._dal, logger=self._log
+            user_id=self._user_id, data_access_layer=self._dal, logger=self._log
         )
 
     def create_admin_api(self) -> AdminApi:
         # TODO validate token for ADMINS
         return AdminApi(
-            user_id=self._user, data_access_layer=self._dal, logger=self._log
+            user_id=self._user_id, data_access_layer=self._dal, logger=self._log
         )
 
     def create_user_api(self) -> UserApi:
         # TODO validate token for REGISTERED USERS
-        return UserApi(user_id=self._user, data_access_layer=self._dal, logger=self._log)
+        return UserApi(user_id=self._user_id, data_access_layer=self._dal, logger=self._log)
