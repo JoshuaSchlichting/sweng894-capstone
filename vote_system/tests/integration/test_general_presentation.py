78f6b3722cee8b0b17766087f60994bf44f2e972
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-07-04 17:32:05 -0400

    added test_get_election_returns_election

--- vote_system/tests/integration/test_general_presentation.py
+++ vote_system/tests/integration/test_general_presentation.py
@@ -4,6 +4,14 @@ from presentation.app import app
 
 
 @pytest.fixture
+def data_access_layer():
+    from mongomock import MongoClient
+    import db_implementation
+    db = db_implementation.MongoDbApi(MongoClient())
+    db.create_user('test', 'test')
+    return db
+
+@pytest.fixture
 def client():
     app.config["TESTING"] = True
     return app.test_client()
@@ -23,4 +31,16 @@ def token(client):
 def test_index_loads(client):
     response = client.get("/")
     assert response.data.endswith(b"html>")
+    assert response.status_code == 200
+
+
+def test_get_election_returns_election(client, mocker, monkeypatch, data_access_layer):
+    dal = mocker.Mock()
+    dal.return_value = data_access_layer
+    monkeypatch.setattr('presentation.app._get_data_access_layer', dal)
+    election_id = dal().create_election("city council 2021")
+    response = client.get(
+        "/election",
+        json={"electionId": election_id}
+    )
     assert response.status_code == 200
