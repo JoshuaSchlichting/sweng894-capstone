e1d77f45760c2e1ce50571995e281c3c0d6a6244
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-07-04 15:50:21 -0400

    added static mock database for test to monkey patch into program

--- vote_system/tests/integration/test_admin_presentation.py
+++ vote_system/tests/integration/test_admin_presentation.py
@@ -17,6 +17,15 @@ def client():
 
 
 @pytest.fixture
+def data_access_layer():
+    from mongomock import MongoClient
+    import db_implementation
+    db = db_implementation.MongoDbApi(MongoClient())
+    db.create_user('test', 'test')
+    return db
+
+
+@pytest.fixture
 def token(client):
     response = client.post(
         "/login", data=dict(inputUsername="test", inputPassword="test")
@@ -27,7 +36,10 @@ def token(client):
     return token
 
 
-def test_create_user(client, token):
+def test_create_user(client, token, data_access_layer, monkeypatch, mocker):
+    dal = mocker.Mock()
+    dal.return_value = data_access_layer
+    monkeypatch.setattr('presentation.app._get_data_access_layer', dal)
     response = client.post(
         "/user", headers=get_admin_headers(token), json={"username": "newuser"}
     )
