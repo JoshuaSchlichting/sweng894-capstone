79b0bb2d5f8f5871884b30029a14ea039436a2a5
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-07-06 22:38:26 -0400

    added get_all_elections() to basicAPI

--- vote_system/app/core/basic_api.py
+++ vote_system/app/core/basic_api.py
@@ -7,3 +7,6 @@ class BasicApi(BaseApi):
 
     def get_election(self, election_id: str) -> dict:
         return self._dal.get_election(election_id)
+
+    def get_all_elections(self) -> list:
+        return self._dal.get_all_elections()
