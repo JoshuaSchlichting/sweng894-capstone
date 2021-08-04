7992e5cf05da6ae332f23c28b990ebd33df319b8
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-08-03 21:25:01 -0400

    added clarifying comment

--- eks/add-pipeline-user-to-eks.sh
+++ eks/add-pipeline-user-to-eks.sh
@@ -1,3 +1,5 @@
+# This is required for the pipeline runner to be able to work with eksctl
+# https://aws.amazon.com/blogs/containers/amazon-eks-cluster-automation-with-gitlab-ci-cd/
 eksctl create iamidentitymapping \
     --cluster fargate-eks  \
     --arn arn:aws:iam::415428590389:user/user-pipeline-1 \
