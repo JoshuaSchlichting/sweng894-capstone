7446ba3906a10b79e762c4521a6bd3626d5645b0
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-08-05 22:46:44 -0400

    added election report to basic api

--- vote_system/app/core/basic_api.py
+++ vote_system/app/core/basic_api.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from .base_api import BaseApi
+from .vote_sys import VoteSystem
 
 
 class BasicApi(BaseApi):
@@ -15,3 +16,8 @@ class BasicApi(BaseApi):
 
     def get_candidates_by_election(self, election_id: str) -> List[dict]:
         return self._dal.get_candidates_by_election(election_id=election_id)
+
+    def get_election_report(self, election_id: str) -> str:
+        self._log.debug(f"Getting election report for election: {election_id}")
+        vote_system = VoteSystem(self._dal, self._log)
+        return vote_system.get_election_report(election_id)
