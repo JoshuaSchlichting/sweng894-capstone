08dc3df18d04e143f11c1806311cc0f0c16c8ef4
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-08-05 23:00:57 -0400

    rm excessive logging

--- vote_system/app/core/vote_sys.py
+++ vote_system/app/core/vote_sys.py
@@ -15,19 +15,13 @@ class VoteSystem:
 
     def get_election_report(self, election_id: str) -> str:
         votes = self._dal.get_all_votes()
-        self._log.debug(f"votes: {votes}")
         ballots = []
         candidates = self._get_candidate_objects(election_id)
-        self._log.debug(f"candidates: {str(candidates)}")
         for vote in votes:
             ranked_candidate_list = []
             for candidate_obj in vote["ranked_candidate_list"]:
-                self._log.debug(f"vote['ranked_candidate_list']: {vote['ranked_candidate_list']}")
-                self._log.debug(f"candidates: {[str(x) for x in candidates]}")
                 # Look, I'm not proud of this. I'm tired.    
-                self._log.debug(f"candidate obj's id: {candidate_obj['id']}")
                 ranked_candidate_list.append(candidates[[str(x) for x in candidates].index(candidate_obj["id"])])
-            self._log.debug(f"ranked_candidate_list: {ranked_candidate_list}")
             ballots.append(Ballot(ranked_candidates=ranked_candidate_list))
         election_result = pyrankvote.instant_runoff_voting(candidates, ballots)
         self._log.debug(election_result)
